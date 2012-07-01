9aaf473594cd006cfefa5b2e342b1a6fa32ab02d
Arié Bénichou <arie.benichou@gmail.com>
2012-07-01 05:34:35 +0200

    Added GPL3 header

--- src/test/java/pocman/matching/MinimumWeightPerfectMatchingTest.java
+++ src/test/java/pocman/matching/MinimumWeightPerfectMatchingTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Arie Benichou
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package pocman.matching;
 
@@ -7,12 +23,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import pocman.graph.UndirectedGraph;
-import pocman.graph.WeightedEdge;
 import pocman.graph.UndirectedGraph.Builder;
-import pocman.matching.MinimumWeightPerfectMatching;
+import pocman.graph.WeightedEdge;
 
 import com.google.common.collect.Maps;
-
 
 public class MinimumWeightPerfectMatchingTest { // TODO à compléter
 
